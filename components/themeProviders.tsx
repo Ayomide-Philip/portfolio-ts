5bcb5fde2acfac264bfe6ab242a7d2138143956b
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-05-05 22:46:26 +0100

    feat: add ThemeProviders component for theme management

---
+++ components/themeProviders.tsx
@@ -0,0 +1,18 @@
+import { ThemeProvider } from "next-themes";
+
+export default function ThemeProviders({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
